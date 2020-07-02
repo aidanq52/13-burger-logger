20eb9b1d23ccaba94bba6b561320e420f9e9804a
Aidan Q <aidanq52@gmail.com>
2020-07-01 22:17:20 -0700

    adding JAWSDB thing

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static("public"));
+// app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
